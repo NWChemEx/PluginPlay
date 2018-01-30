a47a1f399508d6ede6f9a8cf79f510b29f1cb6ad
ryanmrichard <ryanmrichard1@gmail.com>
2018-01-30 16:32:37 -0600

    Wikipedia class works, redo of physical constants

--- NWChemExRuntime/ChemistryRuntime.hpp
+++ NWChemExRuntime/ChemistryRuntime.hpp
@@ -6,10 +6,19 @@ namespace NWXRuntime {
  *         into a one-stop-shop.
  *
  *  This class is meant primarily as an aggregation unit for simplifying
- *  signatures of functions.
+ *  signatures of functions.  All of its member classes have their own tests
+ *  and this class adds no extra features over what they provide.
  */
 struct ChemicalRuntime {
 
+    ///Basis sets come from EMSL's basis set exchange
+    BasisSetRepo bse;
+
+    ///We all know wikipedia is where one goes to get reference data...
+    ReferenceDataRepo wikipedia;
+
+    ///Molecules come from pubchem
+    MoleculeRepo pubchem;
 
 };
 
