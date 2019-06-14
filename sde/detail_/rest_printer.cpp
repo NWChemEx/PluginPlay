c3b8c1d1bb2ca8a07c618ca8486500416365ca93
ryanmrichard <ryanmrichard1@gmail.com>
2019-06-14 16:21:57 -0500

    makes a reST printer class

--- sde/detail_/rest_printer.cpp
+++ sde/detail_/rest_printer.cpp
@@ -1,10 +1,11 @@
 #include "sde/detail_/rest_printer.hpp"
+#include <iterator>
 #include <sstream>
 
 namespace sde::detail_ {
 
 reSTPrinter::reSTPrinter(std::ostream& os, std::string underline_chars,
-                         std::string overline_chars) :
+                         std::string overline_chars) noexcept :
   m_underline_chars_(std::move(underline_chars)),
   m_overline_chars_(std::move(overline_chars)),
   m_os_(os) {}
@@ -30,18 +31,45 @@ void reSTPrinter::finish_section() {
 }
 
 reSTPrinter& reSTPrinter::operator<<(const std::string& message) {
-    unsigned short nchars = 0;
+    /* There's probably a better way to do this without copying things into the
+     * vectors. I copy into the vectors because I need to know how many
+     * sentences/words I have to properly handle termination.
+     */
+    using itr_type = std::istream_iterator<std::string>;
     std::istringstream iss(message);
     std::string s;
-    while(getline(iss, s, ' ')) {
-        if(nchars + s.size() > 80) {
-            m_os_.get() << std::endl;
-            nchars = 0;
-        }
-        m_os_.get() << s << ' ';
-        nchars += s.size() + 1;
-    }
-    m_os_.get() << std::endl;
+    std::vector<std::string> sentences;
+    while(getline(iss, s)) { sentences.emplace_back(std::move(s)); }
+
+    const auto nsent = sentences.size();
+    for(std::size_t si = 0; si < nsent; ++si) { // Loop over sentences
+        // Break lines into words
+        std::istringstream iss2(sentences[si]);
+        std::vector<std::string> words;
+        words.insert(words.end(), itr_type(iss2), itr_type());
+
+        unsigned short nchars = 0;
+        for(const auto& wordi : words) { // Loop over words
+            const unsigned short size = wordi.size();
+            const bool fits           = nchars + size <= 80;
+
+            // If it fits and there's characters we need a space
+            if(nchars > 0 && fits) {
+                m_os_.get() << ' ';
+                ++nchars;
+            } else if(!fits) { // If it doesn't fit put it on next line
+                m_os_.get() << std::endl;
+                nchars = 0;
+            }
+
+            // Print the word
+            m_os_.get() << wordi;
+            nchars += size;
+        } // loop over words
+
+        // Print a newline if we have more lines left
+        if(si < nsent - 1) { m_os_.get() << std::endl; }
+    } // loop over lines
     return *this;
 }
 
