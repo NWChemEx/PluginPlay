fb7a534a278eea3e931de9088f66942ce90aaabe
ryan <ryanmrichard1@gmail.com>
2020-01-29 08:38:54 -0600

    reformats file

--- tests/test_common.hpp
+++ tests/test_common.hpp
@@ -18,7 +18,6 @@ PROPERTY_TYPE_INPUTS(OneIn) {
 }
 PROPERTY_TYPE_RESULTS(OneIn) { return sde::declare_result(); }
 
-
 // Property type for module with one result
 DECLARE_PROPERTY_TYPE(OneOut);
 PROPERTY_TYPE_INPUTS(OneOut) { return sde::declare_input(); }
@@ -35,7 +34,6 @@ PROPERTY_TYPE_RESULTS(TwoOut) {
       .add_field<char>("Result 2");
 }
 
-
 ////////////////////////////////////////////////////////////////////////////////
 //////////                                                            //////////
 //////////                      Modules                               //////////
@@ -44,13 +42,12 @@ PROPERTY_TYPE_RESULTS(TwoOut) {
 
 // Module with no property type
 DECLARE_MODULE(NoPTModule);
-inline MODULE_CTOR(NoPTModule){}
+inline MODULE_CTOR(NoPTModule) {}
 inline MODULE_RUN(NoPTModule, , ) { return results(); }
-
 
 // Module with just a property type
 DECLARE_MODULE(NullModule);
-inline MODULE_CTOR(NullModule){ satisfies_property_type<NullPT>(); }
+inline MODULE_CTOR(NullModule) { satisfies_property_type<NullPT>(); }
 inline MODULE_RUN(NullModule, , ) { return results(); }
 
 // Module with a description "A description"
@@ -67,13 +64,12 @@ inline MODULE_CTOR(CiteModule) {
     satisfies_property_type<NullPT>();
     citation("A citation");
 }
-inline MODULE_RUN(CiteModule, ,) { return results(); }
-
+inline MODULE_RUN(CiteModule, , ) { return results(); }
 
 // A module with int input "Option 1"
 DECLARE_MODULE(NotReadyModule);
 inline MODULE_CTOR(NotReadyModule) { satisfies_property_type<OneIn>(); }
-inline MODULE_RUN(NotReadyModule, ,) { return results(); }
+inline MODULE_RUN(NotReadyModule, , ) { return results(); }
 
 // Has property type int input "Option 1" and another int input "Option 2"
 DECLARE_MODULE(NotReadyModule2);
@@ -81,12 +77,12 @@ inline MODULE_CTOR(NotReadyModule2) {
     satisfies_property_type<OneIn>();
     add_input<int>("Option 2");
 }
-inline MODULE_RUN(NotReadyModule2, ,){ return results(); }
+inline MODULE_RUN(NotReadyModule2, , ) { return results(); }
 
 // One int result "Result 1" set to 4
 DECLARE_MODULE(ResultModule);
 inline MODULE_CTOR(ResultModule) { satisfies_property_type<OneOut>(); }
-inline MODULE_RUN(ResultModule, , ){
+inline MODULE_RUN(ResultModule, , ) {
     auto rv = results();
     return OneOut::wrap_results(rv, int{4});
 }
@@ -101,15 +97,15 @@ inline MODULE_RUN(SubModModule, , ) { return results(); }
 
 // Module using every feature
 DECLARE_MODULE(RealDeal);
-inline MODULE_CTOR(RealDeal){
+inline MODULE_CTOR(RealDeal) {
     satisfies_property_type<NullPT>();
     satisfies_property_type<OneIn>();
     satisfies_property_type<OneOut>();
 
     description("This module is the real deal. It does math stuff like:\n\n"
-                 ".. math::\n\n"
-                 "   \\sum_{i=0}^N i = \\frac{N(N+1)}{2}\n\n"
-                 "Okay it's not that cool...");
+                ".. math::\n\n"
+                "   \\sum_{i=0}^N i = \\frac{N(N+1)}{2}\n\n"
+                "Okay it's not that cool...");
 
     citation("A. Person. *The Best Article*. A Journal "
              "You Have Never Heard Of. 1 (2008).");
