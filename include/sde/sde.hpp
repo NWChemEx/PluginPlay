b0f65d6510a305978786f348747d8267c162fb7f
Ryan Richard <ryanmrichard1@gmail.com>
2021-04-06 14:51:23 -0500

    add facaded to sde.hpp

--- include/sde/sde.hpp
+++ include/sde/sde.hpp
@@ -5,6 +5,6 @@
  *  This is a convenience header to be used by users of the SDE only!!! This
  *  header file will include all of the major SDE classes. Developers
  */
-
+#include "sde/facade_module.hpp"
 #include "sde/lambda_module.hpp"
 #include "sde/module_manager.hpp"
