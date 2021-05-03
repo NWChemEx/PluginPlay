9b6b6170fa526492534a0c6d943cfd69e01d56c8
Murat Keceli <keceli@gmail.com>
2021-05-03 17:34:38 -0500

    Cleanup serializer/deserializer

--- include/sde/detail_/serialization.hpp
+++ include/sde/detail_/serialization.hpp
@@ -26,6 +26,8 @@ public:
     using binary_archive = cereal::BinaryOutputArchive;
     using json_archive   = cereal::JSONOutputArchive;
     using xml_archive    = cereal::XMLOutputArchive;
+    template<typename T, typename Archive>
+    using is_serializable = cereal::traits::is_output_serializable<T, Archive>;
 
     template<typename Archive>
     Serializer(Archive& ar) : m_ar_(&ar) {}
@@ -33,20 +35,20 @@ public:
     template<typename T>
     Serializer& operator<<(T&& obj2serial) {
         if(m_ar_.index() == 0) {
-            auto bar = std::get<binary_archive*>(m_ar_);
-            if constexpr(cereal::traits::is_output_serializable<
-                           T, binary_archive>::value)
+            if constexpr(is_serializable<T, binary_archive>::value) {
+                auto bar = std::get<binary_archive*>(m_ar_);
                 (*bar)(std::forward<T>(obj2serial));
+            }
         } else if(m_ar_.index() == 1) {
-            auto bar = std::get<json_archive*>(m_ar_);
-            if constexpr(cereal::traits::is_output_serializable<
-                           T, json_archive>::value)
+            if constexpr(is_serializable<T, json_archive>::value) {
+                auto bar = std::get<json_archive*>(m_ar_);
                 (*bar)(std::forward<T>(obj2serial));
-        } else if(m_ar_.index() == 1) {
-            auto bar = std::get<xml_archive*>(m_ar_);
-            if constexpr(cereal::traits::is_output_serializable<
-                           T, xml_archive>::value)
+            }
+        } else if(m_ar_.index() == 2) {
+            if constexpr(is_serializable<T, xml_archive>::value) {
+                auto bar = std::get<xml_archive*>(m_ar_);
                 (*bar)(std::forward<T>(obj2serial));
+            }
         }
         return *this;
     }
@@ -66,30 +68,29 @@ public:
     using binary_archive = cereal::BinaryInputArchive;
     using json_archive   = cereal::JSONInputArchive;
     using xml_archive    = cereal::XMLInputArchive;
+    template<typename T, typename Archive>
+    using is_deserializable = cereal::traits::is_input_serializable<T, Archive>;
 
     template<typename Archive>
     Deserializer(Archive& ar) : m_ar_(&ar) {}
 
     template<typename T>
     Deserializer& operator>>(T&& serial2obj) {
-        if(m_ar_.index() == 0 &&
-           cereal::traits::is_input_serializable<T, binary_archive>::value) {
-            auto bar = std::get<binary_archive*>(m_ar_);
-            if constexpr(cereal::traits::is_input_serializable<
-                           T, binary_archive>::value)
+        if(m_ar_.index() == 0) {
+            if constexpr(is_deserializable<T, binary_archive>::value) {
+                auto bar = std::get<binary_archive*>(m_ar_);
                 (*bar)(std::forward<T>(serial2obj));
-        } else if(m_ar_.index() == 1 &&
-                  cereal::traits::is_input_serializable<T,
-                                                        json_archive>::value) {
-            auto bar = std::get<json_archive*>(m_ar_);
-            if constexpr(cereal::traits::is_input_serializable<
-                           T, json_archive>::value)
+            }
+        } else if(m_ar_.index() == 1) {
+            if constexpr(is_deserializable<T, json_archive>::value) {
+                auto bar = std::get<json_archive*>(m_ar_);
                 (*bar)(std::forward<T>(serial2obj));
+            }
         } else if(m_ar_.index() == 2) {
-            auto bar = std::get<xml_archive*>(m_ar_);
-            if constexpr(cereal::traits::is_input_serializable<
-                           T, xml_archive>::value)
+            if constexpr(is_deserializable<T, xml_archive>::value) {
+                auto bar = std::get<xml_archive*>(m_ar_);
                 (*bar)(std::forward<T>(serial2obj));
+            }
         }
         return *this;
     }
