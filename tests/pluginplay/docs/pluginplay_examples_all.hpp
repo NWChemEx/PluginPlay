5242d7fbd503cae129988f7b51ecbb3f05204500
Bert de Jong <wadejong@lbl.gov>
2021-09-03 17:11:07 -0700

    Adding Python infrastructure and docs case

---
+++ tests/pluginplay/docs/pluginplay_examples_all.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+/** @file pluginplay/pluginplay.hpp
+ *
+ *  This is a convenience header to be used by users of the pluginplay only!!!
+ * This header file will include all of the major pluginplay classes. Developers
+ */
+
+#include "point_charge.hpp"
+#include "electric_field.hpp"
+#include "force.hpp"
+~                                                       
