6146ab02d6da8cea05138081558414831faec481
Murat Keceli <keceli@gmail.com>
2021-11-17 17:43:59 -0600

    Inject pz namespace into pluginplay
    Not sure if this is good, or not.

---
+++ include/pluginplay/serialization.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "parallelzone/serialization.hpp"
+namespace pluginplay {
+using pz::BinaryInputArchive;
+using pz::BinaryOutputArchive;
+using pz::get_input_from_output;
+using pz::get_output_from_input;
+using pz::is_deserializable;
+using pz::is_input_archive;
+using pz::is_input_archive_v;
+using pz::is_output_archive;
+using pz::is_output_archive_v;
+using pz::is_serializable;
+using pz::JSONInputArchive;
+using pz::JSONOutputArchive;
+using pz::PortableBinaryInputArchive;
+using pz::PortableBinaryOutputArchive;
+using pz::XMLInputArchive;
+using pz::XMLOutputArchive;
+} // namespace pluginplay
