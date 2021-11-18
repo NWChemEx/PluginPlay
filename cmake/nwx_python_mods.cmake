da9a570f977db57e15c376a5333ef2df0ddeee56
ryanmrichard <ryanmrichard@users.noreply.github.com>
2021-11-18 21:40:38 +0000

    Update file(s) from NWChemEx-Project/DeveloperTools

--- cmake/nwx_python_mods.cmake
+++ cmake/nwx_python_mods.cmake
@@ -86,6 +86,9 @@ function(cppyy_make_python_package)
     if(ENABLE_SCALAPACK)
        list(APPEND include_dirs ${blacspp_SOURCE_DIR}/include ${scalapackpp_SOURCE_DIR}/include)
     endif()
+    if("${install_data_PACKAGE}" STREQUAL "chemist")
+       list(APPEND include_dirs ${CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES})
+    endif()
     file(GENERATE OUTPUT ${python_defines_file} CONTENT "${python_defines}")
     #---------------------------------------------------------------------------
     #-----------------Generate _init__.py file contents------------------------
