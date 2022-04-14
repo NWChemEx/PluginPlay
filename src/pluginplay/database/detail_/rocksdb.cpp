4cbb87aeabd9a53b376f372493c7a14c7acb4958
Ryan Richard <ryanmrichard1@gmail.com>
2022-04-14 15:55:12 -0500

    re-enables public API [skip ci]

--- src/pluginplay/database/detail_/rocksdb.cpp
+++ src/pluginplay/database/detail_/rocksdb.cpp
@@ -14,6 +14,66 @@
 
 namespace pluginplay::database::detail_ {
 
+#define TPARAMS template<typename KeyType, typename ValueType>
+#define ROCKS_DB RocksDB<KeyType, ValueType>
+
+TPARAMS
+ROCKS_DB::RocksDB() noexcept = default;
+
+TPARAMS
+ROCKS_DB::RocksDB(const_path_reference path) :
+  m_pimpl_(std::make_unique<pimpl_type>(path)) {}
+
+TPARAMS
+ROCKS_DB::~RocksDB() noexcept = default;
+
+TPARAMS
+bool ROCKS_DB::count_(const_key_reference key) const noexcept {
+    return pimpl_().count(key);
+}
+
+TPARAMS
+void ROCKS_DB::insert_(key_type key, mapped_type value) {
+    pimpl_().insert(std::move(key), std::move(value));
+}
+
+TPARAMS
+void ROCKS_DB::free_(const_key_reference key) { pimpl_().free(key); }
+
+TPARAMS
+typename ROCKS_DB::const_mapped_reference ROCKS_DB::at_(
+  const_key_reference key) const {
+    return pimpl_().at(key);
+}
+
+TPARAMS
+void ROCKS_DB::backup_() {}
+
+TPARAMS
+void ROCKS_DB::dump_() {}
+
+TPARAMS
+void ROCKS_DB::assert_pimpl_() const {
+    if(m_pimpl_) return;
+    throw std::runtime_error("Object has no PIMPL. Was it default constructed "
+                             "or moved from?");
+}
+
+TPARAMS
+typename ROCKS_DB::pimpl_reference ROCKS_DB::pimpl_() {
+    assert_pimpl_();
+    return *m_pimpl_;
+}
+
+TPARAMS
+typename ROCKS_DB::const_pimpl_reference ROCKS_DB::pimpl_() const {
+    assert_pimpl_();
+    return *m_pimpl_;
+}
+
+#undef ROCKS_DB
+#undef TPARAMS
+
 template class RocksDB<std::string, std::string>;
 
 } // namespace pluginplay::database::detail_
--- src/pluginplay/database/detail_/rocksdb.hpp
+++ src/pluginplay/database/detail_/rocksdb.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "database_pimpl.hpp"
+#include <memory>
 #include <string>
 
 namespace pluginplay::database::detail_ {
@@ -7,6 +8,13 @@ class RocksDBPIMPL;
 
 /** @brief Wraps RocksDB in PluginPlay's database API.
  *
+ *  This DatabasePIMPL can be used to implement a Database whose data is managed
+ *  by RocksDB. The RocksDB class has its own PIMPL which wraps the calls to
+ *  RocksDB, and handles dispatch if the user opted not to build PluginPlay with
+ *  RocksDB support. In the case that PluginPlay was built without RocksDB
+ *  support attempting to use this class will result in runtime errors.
+ *
+ *  @tparam
  */
 template<typename KeyType, typename ValueType>
 class RocksDB : public DatabasePIMPL<KeyType, ValueType> {
@@ -29,25 +37,49 @@ public:
 
     using const_mapped_reference = typename base_type::const_mapped_reference;
 
-    // RocksDB() = default;
+    RocksDB() noexcept;
 
-    // explicit RocksDB(const_path_reference path);
+    ~RocksDB() noexcept;
 
-    // protected:
-    //     bool count_(const_key_reference key) const noexcept override;
+    explicit RocksDB(const_path_reference path);
 
-    //     void insert_(KeyType key, ValueType value) override;
+protected:
+    bool count_(const_key_reference key) const noexcept override;
 
-    //     void free_(const_key_reference key) override;
+    void insert_(key_type key, mapped_type value) override;
 
-    //     const_mapped_reference at_(const_key_reference key) const override;
+    void free_(const_key_reference key) override;
 
-    //     void backup_() override {}
+    const_mapped_reference at_(const_key_reference key) const override;
 
-    //     void dump_() override {}
+    void backup_() override;
 
-    // private:
-    //     std::unique_ptr<RocksDBPIMPL> m_pimpl_;
+    void dump_() override;
+
+private:
+    /// Type of the implementation
+    using pimpl_type = RocksDBPIMPL;
+
+    /// Type of a mutable reference to the PIMPL
+    using pimpl_reference = pimpl_type&;
+
+    /// Type of a read-only reference to the PIMPL
+    using const_pimpl_reference = const pimpl_type&;
+
+    /// Type of a mutable pointer to the PIMPL
+    using pimpl_pointer = std::unique_ptr<pimpl_type>;
+
+    /// Factors out throwing if a PIMPL has not been allocated
+    void assert_pimpl_() const;
+
+    /// Calls assert_pimpl_ then returns a mutable PIMPL
+    pimpl_reference pimpl_();
+
+    /// Calls assert_pimpl_ then returns an immutable PIMPL
+    const_pimpl_reference pimpl_() const;
+
+    /// The actual implementation
+    pimpl_pointer m_pimpl_;
 };
 
 extern template class RocksDB<std::string, std::string>;
