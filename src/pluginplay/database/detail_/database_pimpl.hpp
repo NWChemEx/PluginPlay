99ad9600451216de2f0496910eef236488aba241
Ryan Richard <ryanmrichard1@gmail.com>
2022-04-19 10:59:40 -0500

    RocksDB Class (#239)
    * back-up [skip ci]
    
    * backing-up refactor [skip ci]
    
    * back up for today [skip ci]
    
    * rocksdb_pimpl documented and tested [skip ci]
    
    * re-enables public API [skip ci]
    
    * backup [skip ci]
    
    * back up [skip ci]
    
    * r2g

---
+++ src/pluginplay/database/detail_/database_pimpl.hpp
@@ -0,0 +1,290 @@
+#pragma once
+#include "../value.hpp"
+
+namespace pluginplay::database::detail_ {
+
+/** @brief Implements a Database object.
+ *
+ *  To keep things simple for the user we only have one Database class. This
+ *  class defines a very simple API that all database backends adhere to.
+ *  Different implementations are created by specializing the Database class's
+ *  PIMPL. The DatabasePIMPL class defines the API that all PIMPLs must have.
+ *
+ *  @tparam KeyType In PluginPlay databases are viewed as key/value stores,
+ *                  @p KeyType is the type for the keys.
+ *  @tparam ValueType In PluginPlay databases are viewed as key/value stores,
+ *                    @p ValueType is the type for the values.
+ */
+template<typename KeyType, typename ValueType>
+class DatabasePIMPL {
+public:
+    /// Type used to store the keys in the database, same as @p KeyType
+    using key_type = KeyType;
+
+    /// Read-only reference to a key. Alias for `const KeyType&`
+    using const_key_reference = const key_type&;
+
+    /// Type of the values being stored in the database, same as @p ValueType
+    using mapped_type = ValueType;
+
+    /** @brief mutable reference to a value
+     *
+     *  Because different backends have different memory semantics, insulating
+     *  the user from these semantics requires a more complex solution than
+     *  simply using `ValueType&`. To this end we have created the `Value`
+     *  class. The Value class is capable of holding either a reference to an
+     *  object or the value itself. Calling `.get()` on the value returns a
+     *  mutable reference, regardless of whether the `Value` instance owns the
+     *  mapped value or if the backend does. This type is an alias of
+     *  `Value<ValueType>`.
+     *
+     *  N.B. `mapped_reference` is only used by the backend. The public API of
+     *       DatabasePIMPL uses const_mapped_reference only. Avoiding problems
+     *       related to mutating values owned by `Value` instances.
+     */
+    using mapped_reference = Value<mapped_type>;
+
+    /** @brief immutable reference to a value
+     *
+     *  Because different backends have different memory semantics, insulating
+     *  the user from these semantics requires a more complex solution than
+     *  simply using `const ValueType&`. To this end we have created the
+     *  `ConstValue` class. The ConstValue class is capable of holding either a
+     *  const reference to an object or the value itself. Calling `.get()` on
+     *  the ConstValue instance returns an immutable reference to the wrapped
+     *  value, regardless of whether the `ConstValue` instance owns the
+     *  mapped value or if the backend does. This type is an alias of
+     *  `ConstValue<ValueType>`.
+     *
+     */
+    using const_mapped_reference = ConstValue<mapped_type>;
+
+    /// No-op, no-throw default ctor
+    DatabasePIMPL() noexcept = default;
+
+    /// No-op, no-throw polymorphic default dtor
+    virtual ~DatabasePIMPL() noexcept = default;
+
+    /** @brief Public API for determining if a database contains a key.
+     *
+     *  Databases are viewed as key/value stores. This method is used to
+     *  determine the number of times a given key appears in the database. We
+     *  require that the database keys be unique, which in turn means that the
+     *  result of calling this function is either 0 or 1, i.e., false or true.
+     *
+     *  N.B. This function is actually implemented by count_
+     *
+     *  @return The number of times @p key appears in the database. Since keys
+     *          can only appear once, the return is true if @p key is in the
+     *          database and false otherwise.
+     *
+     *  @throw None No throw guarantee.
+     */
+    bool count(const_key_reference key) const noexcept { return count_(key); }
+
+    /** @brief Public API for adding a key/value pair to the database.
+     *
+     *  Databases are viewed as key/value stores. This method is used to add a
+     *  key/value pair to the database. If @p key is not currently being used as
+     *  a key, then a new entry will be created using @p key as the key and
+     *  @p value as the mapped value. If @p key is currently in use, then
+     *  @p value will overwrite the value currently associated with @p key.
+     *
+     *  N.B. The implementation will own a copy of @p key and @p value. If you
+     *       don't want to give up ownership of @p key and @p value let the
+     *       API make a copy. Otherwise, make sure you `std::move` @p key and
+     *       @p value into this function to avoid (potentially costly) copies.
+     *
+     *  N.B. This function is implemented by insert_
+     *
+     *  @param[in] key The label @p value will be stored under.
+     *  @param[in] value The object being stored under @p key.
+     *
+     *  @throw ??? Throws if the backend throws. Same guarantee as the backend.
+     */
+    void insert(KeyType key, ValueType value);
+
+    /** @brief Public API for releasing a key.
+     *
+     *  This method will delete the specified key and the value associated with
+     *  it. If @p key is not in use this is a no-op. After calling this method
+     *  @p key will not be a valid key for this database (and if applicable, any
+     *  subdatabases). In general calling this method will also invalidate all
+     *  references obtained via `at` or `operator[]`, although it may not happen
+     *  immediately.
+     *
+     *  N.B. This function is implemented by free_
+     *
+     *  @param[in] key The database will forget that this key (and its
+     *                 associated value) ever existed.
+     *
+     *  @throw ??? Throws if the backend throws. Backends are encouraged to
+     *             implement free in a manner which is no-throw guarantee, but
+     *             this may not always be possible.
+     */
+    void free(const_key_reference key) { free_(key); }
+
+    /** @brief Returns the value associated with a key, throwing if the key is
+     *         not valid.
+     *
+     *  This function is the same as operator[], but will throw if there is no
+     *  value stored under @p key.
+     *
+     *  N.B. This function's implementation relies on count_ and at_.
+     *
+     *  @param[in] key The label of the value we want.
+     *
+     *  @return An object whose `.get()` method returns an immutable reference
+     *          to the value associated with @p key.
+     *
+     *  @throw std::out_of_range if @p key is not currently associated with a
+     *                           value in the database. Strong throw guarantee.
+     */
+    const_mapped_reference at(const_key_reference key) const;
+
+    /** @brief Returns the value associated with a key.
+     *
+     *  This method is used to retrieve the value associated with @p key. The
+     *  backend is encouraged to do so without performing a bounds check (i.e.,
+     *  ensuring @p key is assigned to a value) so that users can prefer this
+     *  method over `at` when they know that @p key is valid. That said,
+     *  depending on the backend's implementation, looking for the @p key can
+     *  itself amount to a bounds check. In these cases the backend may still
+     *  throw if the @p key is not found. Even if the backend doesn't throw,
+     *  attempting to use the value returned by an invalid key is undefined
+     *  behavior.
+     *
+     *  @param[in] key The label of the value we want.
+     *
+     *  @return An object whose `.get()` method returns an immutable reference
+     *          to the value associated with @p key.
+     *
+     *  @throw ??? Throws if the backend throws. Same throw guarantee.
+     */
+    const_mapped_reference operator[](const_key_reference key) const;
+
+    /** @brief Checkpoints the database.
+     *
+     *  This function is called when the user wants the database to be
+     *  checkpointed, but is not done with the data in the database. Think of
+     *  this as "save". Exactly what this means is up to the derived class, but
+     *  in general after a successful call to this method, the database instance
+     *  will still have the same keys and values, but the data in it will be
+     *  backed-up.
+     *
+     * N.B. This function's implementation relies on backup_
+     *
+     * @throw ??? Throws if the backend throws. The backend is encouraged to try
+     *            to implement this method in a no throw manner if at all
+     *            possible.
+     */
+    void backup() { backup_(); }
+
+    /** @brief Checkpoints and erases the database's contents.
+     *
+     *  This method is intended to be more or less backup(), followed by freeing
+     *  up the key/values in the database. The actual data is stored and can be
+     *  reloaded and the memory is freed up. This is it's own function (and not
+     *  implemented in terms of backup_ and free_) so that the backend can
+     *  guarantee that data isn't loss.
+     *
+     *  N.B. This function's implementation relies on dump_
+     *
+     *  @throw ??? Throws if the backend throws. The backend is encouraged to
+     *             implement this in a no throw manner if at all possible.
+     */
+    void dump() { dump_(); }
+
+protected:
+    /** @brief Hook for derived class to implement count.
+     *
+     *  The derived class is responsible for overriding this method with a
+     *  definition consistent with the description of DatabasePIMPL::count.
+     *
+     *  @param[in] key The key the user is looking for.
+     *
+     *  @return True if the key is found and false otherwise.
+     *
+     *  @throw None No throw guarantee.
+     */
+    virtual bool count_(const_key_reference key) const noexcept = 0;
+
+    /** @brief Hook for derived class to implement insert
+     *
+     *  The derived class is responsible for overriding this method with a
+     *  definition consistent with the description of DatabasePIMPL::insert.
+     *
+     *  @param[in] key The key that @p value will be stored under.
+     *  @param[in] value The object to store under @p key.
+     *
+     *  @throw ??? Backend may choose to throw for a variety of reasons, many of
+     *             which can not be anticipated by this API.
+     */
+    virtual void insert_(KeyType key, ValueType value) = 0;
+
+    /** @brief Hook for derived class to implement free
+     *
+     *  The derived class is responsible for overriding this method with a
+     *  definition consistent with the description of DatabasePIMPL::free.
+     *
+     *  @param[in] key The key for the key/value pair to delete.
+     *
+     *  @throw ??? The backend may choose to throw if appropriate.
+     */
+    virtual void free_(const_key_reference key) = 0;
+
+    /** @brief Hook for derived class to implement operator[]
+     *
+     *  The derived class is responsible for overriding this method with a
+     *  definition consistent with the description of DatabasePIMPL::operator[].
+     *
+     *  @param[in] key The key whose associated value will be returned.
+     *
+     *  @throw ??? The backend may choose to throw if appropriate.
+     */
+    virtual const_mapped_reference at_(const_key_reference key) const = 0;
+
+    /** @brief Hook for derived class to implement backup
+     *
+     *  The derived class is responsible for overriding this method with a
+     *  definition consistent with the description of DatabasePIMPL::backup.
+     *
+     *  @throw ??? The backend may choose to throw if appropriate.
+     */
+    virtual void backup_() = 0;
+
+    /** @brief Hook for derived class to implement dump
+     *
+     *  The derived class is responsible for overriding this method with a
+     *  definition consistent with the description of DatabasePIMPL::dump.
+     *
+     *  @throw ??? The backend may choose to throw if appropriate.
+     */
+    virtual void dump_() = 0;
+};
+
+#define TPARAMS template<typename KeyType, typename ValueType>
+#define DB_PIMPL DatabasePIMPL<KeyType, ValueType>
+
+TPARAMS
+void DB_PIMPL::insert(KeyType key, ValueType value) {
+    insert_(std::move(key), std::move(value));
+}
+
+TPARAMS
+typename DB_PIMPL::const_mapped_reference DB_PIMPL::at(
+  const_key_reference key) const {
+    if(count(key)) return at_(key);
+    throw std::out_of_range("Key was not found in the database");
+}
+
+TPARAMS
+typename DB_PIMPL::const_mapped_reference DB_PIMPL::operator[](
+  const_key_reference key) const {
+    return at_(key);
+}
+
+#undef TPARAMS
+#undef DB_PIMPL
+
+} // namespace pluginplay::database::detail_
---
+++ src/pluginplay/database/detail_/rocksdb.cpp
@@ -0,0 +1,80 @@
+/* RocksDB is designed to be an optional dependency. To accomplish this we need
+ * to use preprocessor macros. Our goal is to localize them as much as possible.
+ * To that end, we define two PIMPL implementations, rocksdb_pimpl.hpp contains
+ * the real one which gets included when BUILD_ROCKS_DB is defined and
+ * rocksdb_pimpl_stub.hpp gets included otherwise. The dispatch between the two
+ * happens here (away from the API of the RocksDB class).
+ */
+
+#ifdef BUILD_ROCKS_DB
+#include "rocksdb_pimpl.hpp"
+#else
+#include "rocksdb_pimpl_stub.hpp"
+#endif
+
+namespace pluginplay::database::detail_ {
+
+#define TPARAMS template<typename KeyType, typename ValueType>
+#define ROCKS_DB RocksDB<KeyType, ValueType>
+
+TPARAMS
+ROCKS_DB::RocksDB() noexcept = default;
+
+TPARAMS
+ROCKS_DB::RocksDB(const_path_reference path) :
+  m_pimpl_(std::make_unique<pimpl_type>(path)) {}
+
+TPARAMS
+ROCKS_DB::~RocksDB() noexcept = default;
+
+TPARAMS
+bool ROCKS_DB::count_(const_key_reference key) const noexcept {
+    if(!m_pimpl_) return false;
+    return m_pimpl_->count(key);
+}
+
+TPARAMS
+void ROCKS_DB::insert_(key_type key, mapped_type value) {
+    pimpl_().insert(std::move(key), std::move(value));
+}
+
+TPARAMS
+void ROCKS_DB::free_(const_key_reference key) { pimpl_().free(key); }
+
+TPARAMS
+typename ROCKS_DB::const_mapped_reference ROCKS_DB::at_(
+  const_key_reference key) const {
+    return pimpl_().at(key);
+}
+
+TPARAMS
+void ROCKS_DB::backup_() {}
+
+TPARAMS
+void ROCKS_DB::dump_() {}
+
+TPARAMS
+void ROCKS_DB::assert_pimpl_() const {
+    if(m_pimpl_) return;
+    throw std::runtime_error("Object has no PIMPL. Was it default constructed "
+                             "or moved from?");
+}
+
+TPARAMS
+typename ROCKS_DB::pimpl_reference ROCKS_DB::pimpl_() {
+    assert_pimpl_();
+    return *m_pimpl_;
+}
+
+TPARAMS
+typename ROCKS_DB::const_pimpl_reference ROCKS_DB::pimpl_() const {
+    assert_pimpl_();
+    return *m_pimpl_;
+}
+
+#undef ROCKS_DB
+#undef TPARAMS
+
+template class RocksDB<std::string, std::string>;
+
+} // namespace pluginplay::database::detail_
