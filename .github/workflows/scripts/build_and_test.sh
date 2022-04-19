e9d3dd8e8f0bfd9380262a212d82e33d3faafcfc
ryanmrichard <ryanmrichard@users.noreply.github.com>
2022-04-19 13:47:12 +0000

    Update file(s) from NWChemEx-Project/DeveloperTools

--- .github/workflows/scripts/build_and_test.sh
+++ .github/workflows/scripts/build_and_test.sh
@@ -53,12 +53,7 @@ echo "set(BUILD_TESTING ON)" > "${toolchain_file}"
 } >> "${toolchain_file}"
 
 #Step 2: Configure
-if command -v ninja &> /dev/null
-then
-  ${cmake_command} -GNinja -H. -Bbuild -DCMAKE_TOOLCHAIN_FILE="${toolchain_file}"
-else
-  ${cmake_command} -H. -Bbuild -DCMAKE_TOOLCHAIN_FILE="${toolchain_file}"
-fi
+${cmake_command} -H. -Bbuild -DCMAKE_TOOLCHAIN_FILE="${toolchain_file}"
 
 #Step 3: Compile
 ${cmake_command} --build build
