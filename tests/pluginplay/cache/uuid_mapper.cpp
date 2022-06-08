839058763faeba9af3e6e0136317fb0fa1db7e0e
ryan <ryanmrichard1@gmail.com>
2022-06-08 09:56:11 -0500

    backup

--- tests/pluginplay/cache/uuid_mapper.cpp
+++ tests/pluginplay/cache/uuid_mapper.cpp
@@ -1,23 +1,20 @@
 #include "lexical_cast.hpp"
+#include "test_cache.hpp"
 #include <catch2/catch.hpp>
 #include <iostream>
 #include <pluginplay/cache/database/native.hpp>
 #include <pluginplay/cache/uuid_mapper.hpp>
+#include <pluginplay/types.hpp>
 
 using namespace pluginplay::cache;
 using namespace pluginplay::cache::database;
 
-using test_types = std::tuple<int, char, std::string>;
-
-TEMPLATE_LIST_TEST_CASE("UUIDMapper", "", test_types) {
+TEMPLATE_LIST_TEST_CASE("UUIDMapper", "", testing::test_types) {
     using key_type     = TestType;
     using uuid_db_type = UUIDMapper<key_type>;
     using uuid_type    = typename uuid_db_type::mapped_type;
-    using db_type      = Native<key_type, uuid_type>;
 
-    auto inner_db = std::make_unique<db_type>();
-    auto pinner   = inner_db.get();
-    auto db       = std::make_unique<db_type>(std::move(inner_db));
+    auto [pinner, db] = testing::make_nested_native<key_type, uuid_type>();
 
     key_type key0;
     auto key1 = testing::lexical_cast<key_type>("42");
@@ -72,4 +69,17 @@ TEMPLATE_LIST_TEST_CASE("UUIDMapper", "", test_types) {
         REQUIRE_FALSE(uuid_db.count(key0));
         REQUIRE(pinner->count(key0));
     }
+}
+
+TEST_CASE("UUIDMapper<AnyField>") {
+    using any_type     = pluginplay::type::any;
+    using uuid_db_type = UUIDMapper<any_type>;
+    using uuid_type    = typename uuid_db_type::mapped_type;
+
+    auto [psub_sub_sub, psub_sub, sub] =
+      testing::make_transposer<any_type, uuid_type>();
+
+    auto sub_db = std::make_unique<decltype(sub)>(std::move(sub));
+    auto psub   = sub_db.get();
+    uuid_db_type db(std::move(sub_db));
 }
