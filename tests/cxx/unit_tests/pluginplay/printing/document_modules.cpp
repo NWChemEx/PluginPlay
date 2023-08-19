cf44a44575b27c0a9c8abe2562dec04ce75d05da
Murat Keceli <keceli@gmail.com>
2023-08-18 23:30:09 -0500

    Remove doc_snippet test from unit_tests

--- tests/cxx/unit_tests/pluginplay/printing/document_modules.cpp
+++ tests/cxx/unit_tests/pluginplay/printing/document_modules.cpp
@@ -19,49 +19,35 @@
 #include <iterator>
 #include <pluginplay/printing/document_modules.hpp>
 
-#include "../doc_snippets/load_modules.hpp"
-
 TEST_CASE("document_modules") {
-    // Need a docs path
-    const auto root_dir = std::filesystem::temp_directory_path();
-    const std::filesystem::path docs_dir("docs");
-    auto docs_path = root_dir / docs_dir;
+   // Need a docs path
+   const auto root_dir = std::filesystem::temp_directory_path();
+   const std::filesystem::path docs_dir("docs");
+   auto docs_path = root_dir / docs_dir;
 
-    std::filesystem::remove_all(docs_path);
+   std::filesystem::remove_all(docs_path);
 
-    // Create the directory
-    std::filesystem::create_directory(docs_path);
+   // Create the directory
+   std::filesystem::create_directory(docs_path);
 
-    // Verify the directory exists
-    REQUIRE(std::filesystem::exists(docs_path));
-    REQUIRE(std::filesystem::is_directory(docs_path));
-    REQUIRE(std::distance(std::filesystem::directory_iterator(docs_path),
-                          std::filesystem::directory_iterator{}) ==
-            0); // Empty directory
+   // Verify the directory exists
+   REQUIRE(std::filesystem::exists(docs_path));
+   REQUIRE(std::filesystem::is_directory(docs_path));
+   REQUIRE(std::distance(std::filesystem::directory_iterator(docs_path),
+                         std::filesystem::directory_iterator{}) ==
+           0); // Empty directory
 
-    // Set up module manager
-    pluginplay::ModuleManager mm;
+   // Set up module manager
+   pluginplay::ModuleManager mm;
 
-    SECTION("Empty Module Manager") {
-        int correct = 1; // for index.html
+   SECTION("Empty Module Manager") {
+       int correct = 1; // for index.html
 
-        // Generate module documentation at the given path
-        pluginplay::printing::document_modules(mm, docs_path);
+       // Generate module documentation at the given path
+       pluginplay::printing::document_modules(mm, docs_path);
 
-        REQUIRE(std::distance(std::filesystem::directory_iterator(docs_path),
-                              std::filesystem::directory_iterator{}) ==
-                correct);
-    }
-
-    SECTION("Modules Loaded") {
-        // Load your modules
-        pluginplay_examples::load_modules(mm);
-
-        // Generate module documentation at the given path
-        pluginplay::printing::document_modules(mm, docs_path);
-
-        REQUIRE(std::distance(std::filesystem::directory_iterator(docs_path),
-                              std::filesystem::directory_iterator{}) ==
-                mm.size() + 1); // +1 for index.rst
-    }
+       REQUIRE(std::distance(std::filesystem::directory_iterator(docs_path),
+                             std::filesystem::directory_iterator{}) ==
+               correct);
+   }
 }
