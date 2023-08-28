aac1f08e4eabd052ed27642ffccca92cea61066c
Clang Robot <robot@example.com>
2023-08-28 20:42:38 +0000

    Committing clang-format changes

--- tests/cxx/integration_tests/blank_test.cpp
+++ tests/cxx/integration_tests/blank_test.cpp
@@ -1,13 +1,11 @@
 #define CATCH_CONFIG_MAIN
-#include <iostream>
 #include <catch2/catch.hpp>
+#include <iostream>
 
 int print_and_return(int ii) {
-    std::cout<<"This is only a blank model for an integration test."<<std::endl;
+    std::cout << "This is only a blank model for an integration test."
+              << std::endl;
     return 0;
 }
 
-TEST_CASE("Blank test", "[classic]")
-{
-   REQUIRE(print_and_return(0) == 0);
-}
+TEST_CASE("Blank test", "[classic]") { REQUIRE(print_and_return(0) == 0); }
