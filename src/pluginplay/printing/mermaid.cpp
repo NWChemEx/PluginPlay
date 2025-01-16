a06ae221e8df126b6dce6c40b0479b3a15f3f21f
Jacob Heflin <jlheflin@iastate.edu>
2025-01-16 09:31:53 -0600

    added some debug printout

--- src/pluginplay/printing/mermaid.cpp
+++ src/pluginplay/printing/mermaid.cpp
@@ -17,14 +17,28 @@
 #include <pluginplay/pluginplay.hpp>
 
 std::string hello_world(const pluginplay::ModuleManager& mm) {
+    std::cout << "RUNNING IN THE SOURCE" << std::endl;
+    std::cout << "ASSIGNING mm.size() TO n_modules" << std::endl;
     auto n_modules = mm.size();
+    std::cout << "RUNNING FOR LOOP 1" << std::endl;
     for(decltype(n_modules) i = 0; i < n_modules; i++) {
+        std::cout << "ASSIGNING m.keys()[i] TO mod" << std::endl;
         auto mod    = mm.keys()[i];
+        std::cout << "ASSIGNING mm.at(mod) TO module" << std::endl;
         auto module = mm.at(mod);
+        std::cout << "PRINTING mod" << std::endl;
         std::cout << mod << std::endl;
+        std::cout << "ASSIGNING module.submods() TO submods" << std::endl;
         auto submods = module.submods();
-        for(const auto& [key, value] : submods)
-            std::cout << "This is a submod: " << key << std::endl;
+        std::cout << "RUNNING FOR LOOP 2" << std::endl;
+        for(const auto& [key, value] : submods) {
+            std::cout << "PRINTING key IN submods" << std::endl;
+            std::cout << "This is a submod identifier: " << key << std::endl;
+            std::cout << "ASSIGNING mm.at(key).get_name() TO submod_name" << std::endl;
+            auto submod_name = mm.at(key).get_name();
+            std::cout << "PRINTING submod_name" << std::endl;
+            std::cout << "This is the true name: " << submod_name << std::endl;
+        }
     }
     return "Hello World!";
 }
