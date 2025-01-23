8c9a575d6e636b8054c78693621057343e6a6736
Jacob Heflin <jlheflin@iastate.edu>
2025-01-23 11:18:25 -0600

    Really close now

--- src/pluginplay/printing/mermaid.cpp
+++ src/pluginplay/printing/mermaid.cpp
@@ -18,45 +18,32 @@
 #include "module_manager/module_manager_class.hpp"
 #include <pluginplay/pluginplay.hpp>
 
-void print_submods(const pluginplay::Module& module, int level = 0) {
-    for(int i = 0; i < level; ++i) { std::cout << "    "; }
-    auto submods = module.submods();
-
-    for(const auto& [key, value] : submods) {
-        if(value.ready() == false) { continue; }
-        auto submod_name = value.get_name();
-        std::cout << "Submod Name: " << submod_name << std::endl;
-
-        pluginplay::ModuleManager mm;
-
-        for(const auto& [key, submod] : mm.at(submod_name).submods()) {
-            print_submods(submod, level + 1);
-        }
+void print_submods(const std::string module, int level = 0) {
+    for (int i = 0; i < level; ++i) {
+        std::cout << "    ";
     }
+    pluginplay::ModuleManager mm;
+    auto mm_module = mm.at(module);
+    auto submods = mm_module.submods();
+
+    for (const auto& [key, value] : submods) {
+        if (value.ready() == false) {
+            continue;
+        }
+        std::cout << "Submod ID: "<< key << std::endl;
+        std::cout << "Submod Name: " << value.get_name() << std::endl;
+
+        std::string submod_name = value.get_name();
+        
+        print_submods(submod_name, level + 1);
+        }
 }
 
 std::string hello_world(const pluginplay::ModuleManager& mm) {
     auto n_modules = mm.size();
     for(decltype(n_modules) i = 0; i < n_modules; i++) {
         auto mod    = mm.keys()[i];
-        auto module = mm.at(mod);
-
-        print_submods(module);
-
-        // std::cout << mod << std::endl;
-
-        // auto submods = module.submods();
-
-        // for(const auto& [key, value] : submods) {
-        //     std::cout << "This is a submod identifier: " << key << std::endl;
-
-        //     auto submod_name = value.get_name();
-
-        //     std::cout << "This is the true name: " << submod_name <<
-        //     std::endl;
-
-        //     auto mm_submod = mm.at(submod_name);
-        //}
+        print_submods(mod);
     }
     return "Hello World!";
 }
