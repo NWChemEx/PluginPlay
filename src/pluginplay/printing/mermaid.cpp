40d249e54dfc25d8d52801eb1c3d523dfe970efd
Jacob Heflin <jlheflin@iastate.edu>
2025-01-23 10:56:44 -0600

    In git commit purgatory, making sure I didn't kill anything

--- src/pluginplay/printing/mermaid.cpp
+++ src/pluginplay/printing/mermaid.cpp
@@ -18,31 +18,6 @@
 #include "module_manager/module_manager_class.hpp"
 #include <pluginplay/pluginplay.hpp>
 
-<<<<<<< HEAD
-std::string hello_world(const pluginplay::ModuleManager& mm) {
-    std::cout << "RUNNING IN THE SOURCE" << std::endl;
-    std::cout << "ASSIGNING mm.size() TO n_modules" << std::endl;
-    auto n_modules = mm.size();
-    std::cout << "RUNNING FOR LOOP 1" << std::endl;
-    for(decltype(n_modules) i = 0; i < n_modules; i++) {
-        std::cout << "ASSIGNING m.keys()[i] TO mod" << std::endl;
-        auto mod = mm.keys()[i];
-        std::cout << "ASSIGNING mm.at(mod) TO module" << std::endl;
-        auto module = mm.at(mod);
-        std::cout << "PRINTING mod" << std::endl;
-        std::cout << mod << std::endl;
-        std::cout << "ASSIGNING module.submods() TO submods" << std::endl;
-        auto submods = module.submods();
-        std::cout << "RUNNING FOR LOOP 2" << std::endl;
-        for(const auto& [key, value] : submods) {
-            std::cout << "PRINTING key IN submods" << std::endl;
-            std::cout << "This is a submod identifier: " << key << std::endl;
-            std::cout << "ASSIGNING mm.at(key).get_name() TO submod_name"
-                      << std::endl;
-            auto submod_name = mm.at(key).get_name();
-            std::cout << "PRINTING submod_name" << std::endl;
-            std::cout << "This is the true name: " << submod_name << std::endl;
-=======
 void print_submods(const pluginplay::Module& module, int level = 0) {
     for (int i = 0; i < level; ++i) {
         std::cout << "    ";
@@ -52,7 +27,6 @@ void print_submods(const pluginplay::Module& module, int level = 0) {
     for (const auto& [key, value] : submods) {
         if (value.ready() == false) {
             continue;
->>>>>>> 76e1bf2d (Added submodule printing function, adjusted module creation)
         }
         auto submod_name = value.get_name();
         std::cout << "Submod Name: " << submod_name << std::endl;
